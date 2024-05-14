f75ee8d835cabf078043c2f522464bd5b7828872
cevizenes <cevizenes01@gmail.com>
2024-05-14 22:19:35 +0300

    fixed news screen bug

--- app/src/main/java/com/example/newsapp/ui/navigation/Nav.kt
+++ app/src/main/java/com/example/newsapp/ui/navigation/Nav.kt
@@ -24,12 +24,10 @@ import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.newsapp.ui.screen.NewsScreenRoute
 import com.example.newsapp.ui.screen.Screens
 
